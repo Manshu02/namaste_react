9de44ad70b1593a7bd1454719d805678e8f47edb
Mayank Sharma <mayanksharma18567@gmail.com>
2024-07-13 16:30:50 +0530

    starting the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const heading =React.createElement('h1',{id:'txt'},"Hello Mayank This Side")
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {heading}
+    
     </div>
+    
+    
   );
 }
+console.log(heading)
+
+
 
 export default App;
